21f2edc67807b32bfb6a6cbbae3560cdc535de1f
Chikezie Simon <simonchikezie44@gmail.com>
2025-09-10 12:12:19 +0100

    Implement feature updates

--- src/api/github.ts
+++ src/api/github.ts
@@ -4,3 +4,54 @@ export const fetchGithubUser = async (username: string) => {
   const data = await res.json();
   return data;
 };
+
+export const searchGithubUser = async (query: string) => {
+  const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}`);
+  if (!res.ok) throw new Error(`User not found`);
+  const data = await res.json();
+  return data.items;
+};
+
+export const followGitHubUser = async (username: string) => {
+  const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`, {
+    method: 'PUT',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github+json',
+    },
+  });
+  if (!res.ok) throw new Error(`Failed to follow user`);
+  return true;
+};
+
+export const checkIfFollowingUser = async (username: string) => {
+  const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`, {
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+      Accept: 'application/vnd.github+json',
+    },
+  });
+
+  if (res.status === 204) {
+    return true; // Following
+  } else if (res.status === 404) {
+    return false; // Not following
+  } else {
+    const errorData = await res.json().catch(() => null);
+    throw new Error(errorData?.message || 'Failed to check follow status');
+  }
+};
+
+export const unFollowGitHubUser = async (username: string) => {
+  const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/user/following/${username}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github+json',
+    },
+  });
+  if (!res.ok) throw new Error(`Failed to follow user`);
+  return true;
+};
--- src/components/user-search.tsx
+++ src/components/user-search.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { useState, useEffect } from 'react';
-import { fetchGithubUser } from '../api/github';
+import { fetchGithubUser, searchGithubUser } from '../api/github';
 import UserCard from './user-card';
-
 import RecentSearches from './recent-searches';
+import { useDebounce } from 'use-debounce';
+
+import SuggestionDropdown from './suggetions-dropdown';
 
 const UserSearch = () => {
   const [username, setUsername] = useState('');
@@ -12,10 +14,21 @@ const UserSearch = () => {
     const stored = localStorage.getItem('recent');
     return stored ? JSON.parse(stored) : [];
   });
-  const { data, isLoading, isError, error } = useQuery({
+  const [debouncedUsername] = useDebounce(username, 300);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  // query to search specific users
+  const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: ['users', submittedUserName],
     queryFn: () => fetchGithubUser(submittedUserName),
     enabled: !!submittedUserName,
+  });
+
+  // query to fetch suggetions for the userSearch
+  const { data: suggestions } = useQuery({
+    queryKey: ['github-user-suggestions', debouncedUsername],
+    queryFn: () => searchGithubUser(debouncedUsername),
+    enabled: debouncedUsername.length > 1,
   });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -23,6 +36,7 @@ const UserSearch = () => {
     const trimmed = username.trim();
     if (!trimmed) return;
     setSubmittedUsername(trimmed);
+    setUsername('');
     setRecentUsers((prev) => {
       const updated = [trimmed, ...prev.filter((u) => u !== trimmed)];
       return updated.slice(0, 5);
@@ -35,12 +49,37 @@ const UserSearch = () => {
   return (
     <>
       <form onSubmit={handleSubmit} className="form">
-        <input
-          type="text"
-          placeholder="Enter Github Username..."
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <div className="dropdown-wrapper">
+          <input
+            type="text"
+            placeholder="Enter Github Username..."
+            value={username}
+            onChange={(e) => {
+              const value = e.target.value;
+              setUsername(value);
+              setShowSuggestions(value.length > 1);
+            }}
+            onBlur={() => {
+              setTimeout(() => setShowSuggestions(false), 100);
+            }}
+          />
+          {showSuggestions && suggestions?.length > 0 && (
+            <SuggestionDropdown
+              suggestions={suggestions}
+              show={showSuggestions}
+              onSelect={(selected) => {
+                setUsername(selected);
+                setShowSuggestions(false);
+                if (submittedUserName !== selected) {
+                  setSubmittedUsername(selected);
+                } else {
+                  refetch();
+                }
+              }}
+            />
+          )}
+        </div>
+
         <button type="submit">Search</button>
         {isLoading && <p className="status">Loading...</p>}
         {isError && <p className="status error">{error.message}</p>}
